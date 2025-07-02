69f9981a6d754768a3db4432fd6c85ce87b06f47
Wilson Ndirangu <wnmbugua.it@gmail.com>
2025-07-02 12:16:09 +0300

    [REF:] use 'res.config.settings' to enable activation of VSCU signing

--- models/pos_config.py
+++ models/pos_config.py
@@ -9,3 +9,8 @@ class PosConfig(models.Model):
     _inherit = 'pos.config'
     
     vscu_config = fields.Boolean('Send to VSCU?', default=False)
+    
+class ResConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
+    
+    pos_vscu_config = fields.Boolean(related='pos_config_id.vscu_config', readonly=False)
