d9ecda44338e821d28b5745578e9e315145311ce
Wilson Ndirangu <wnmbugua.it@gmail.com>
2025-07-03 15:28:33 +0300

    [ADD:] display errors ona running pos session; get vscu return response

--- static/src/overides/screens/payment/payment_screen.js
+++ static/src/overides/screens/payment/payment_screen.js
@@ -2,42 +2,54 @@
 
 import { PaymentScreen } from "@point_of_sale/app/screens/payment_screen/payment_screen";
 import { patch } from "@web/core/utils/patch";
-import { useService } from "@web/core/utils/hooks";
+import { _t } from "@web/core/l10n/translation";
+import { ErrorPopup } from "@point_of_sale/app/errors/popups/error_popup";
 
 patch(PaymentScreen.prototype, {
+
+    _showError(msg, title) {
+        if (!title) {
+            title = _t("VSCU Error");
+        }
+        this.env.services.popup.add(ErrorPopup, {
+            title: title,
+            body: msg,
+        });
+    },
     
     async getVscuData(){
+        var self = this;
         const order = this.currentOrder;
         const orderName = order.get_name();
         const order_server_id = this.pos.validated_orders_name_server_id_map[orderName];
 
-        await this.orm.call('pos.order', 'action_vscu_sale', [[order_server_id]]);
+        let vscuData = await this.orm.call('pos.order', 'action_vscu_sale', [[order_server_id]]);
 
+        if (vscuData.hasError) {
+            return this._showError(vscuData.message);
+        }
+        
         return new Promise((resolve, reject) => {
-            this.orm.searchRead("pos.order", [['pos_reference', '=', this.currentOrder.name]], ['vscu_qr_code', 'vscu_date', 'vscu_data'])
+            this.orm.searchRead("pos.order", [['pos_reference', '=', order.name]], ['vscu_qr_code', 'vscu_date', 'vscu_data'])
             .then((data) => {
+                if (Object.keys(data).length !== 0) {
+                    order.vscu_qr_code = data[0].vscu_qr_code;
+                    order.vscu_date = data[0].vscu_date;
+                    order.vscu_cu = data[0].vscu_serial;
+                    order.vscu_invoice = data[0].vscu_cu;
+                    order.vscu_data = data[0].vscu_data;
+                }
                 resolve(data);
             })
             .catch((err) => {
+                self._showError(err);
                 reject(err);
             })
         }); 
     },
 
-    async afterOrderValidation(suggestToSync = true) {
-        let vscu;
-        try {
-            vscu = await this.getVscuData();
-            
-            this.currentOrder.vscu_qr_code = vscu[0].vscu_qr_code;
-            this.currentOrder.vscu_date = vscu[0].vscu_date;
-            this.currentOrder.vscu_cu = vscu[0].vscu_data.sdcId.split('/')[0];
-            this.currentOrder.vscu_invoice = vscu[0].vscu_data.sdcId;
-            this.currentOrder.vscu_data = vscu[0].vscu_data.intrlData;
-            
-        } catch (error) {
-            throw error;
-        }
+    async afterOrderValidation(suggestToSync = true) {        
+        await this.getVscuData();       
         await super.afterOrderValidation(...arguments);
     },
 
